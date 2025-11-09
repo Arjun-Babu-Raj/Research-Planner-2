ebd0be1f270d36000ea663e143ea079ffd2c6c84
Arjun-Babu-Raj <arjun.unni16@gmail.com>
2025-11-09 17:51:03 +0530

    Re-add appDir, distDir, and output to next.config.js for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  distDir: '.next',
+  output: 'standalone',
+};
 
 module.exports = nextConfig;
